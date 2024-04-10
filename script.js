893e66275617c1388d4490ef7dc47b8c6a770391
Hom Dahal <homdahal997@gmail.com>
2024-04-10 16:50:05 -0400

    Variables initialized

---
+++ script.js
@@ -0,0 +1,8 @@
+// constants
+const gardenRadius = 5;
+const PI = 3.1415;
+const gardenArea = PI * gardenRadius * gardenRadius;
+
+const numberOfPlants = 20;
+const spaceForEachPlants = 0.8;
+let totalAreaForPlants = numberOfPlants * spaceForEachPlants;
