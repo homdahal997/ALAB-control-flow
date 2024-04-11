124e3ebb5f27726c6d747f4e8cd208e39cfe7744
Hom Dahal <homdahal997@gmail.com>
2024-04-10 20:16:34 -0400

    First week logic implemented

--- script.js
+++ script.js
@@ -2,7 +2,20 @@
 const gardenRadius = 5;
 const PI = 3.1415;
 const gardenArea = PI * gardenRadius * gardenRadius;
-
-const numberOfPlants = 20;
+// maximun number of plants that garden can hold
 const spaceForEachPlants = 0.8;
-let totalAreaForPlants = numberOfPlants * spaceForEachPlants;
+const maxNumberOfPlants = gardenArea/spaceForEachPlants;
+
+// Initial number of plants
+const startingPlantNumber = 20;
+
+// After First week plants double
+let plantCountAfterWeek1 = startingPlantNumber * 2;
+if (plantCountAfterWeek1 > 0.8 * maxNumberOfPlants){
+    console.log("Please prun to prevent from exceding maximum capacity");
+}else if(plantCountAfterWeek1 <= 0.8 * maxNumberOfPlants && plantCountAfterWeek1 >= 0.5 * maxNumberOfPlants){
+    console.log("Monitor the growth of plants");
+}else{
+    console.log("There is room for more plants.");
+}
+
